a597e10166225ad81bc2a87ab37aeb12c212201e
customaddone <custom1485@docomo.ne.jp>
2019-11-20 13:30:50 +0000

    Use scss

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -11,6 +11,9 @@
     <script src="https://unpkg.com/vue@2.5.17"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue-router@3.0.1"></script>
+
+    <!-- Scss（全ページ共通）-->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     @yield('content')
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,14 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+　/*
+    ① npm install
+    ② resources/sass以下にscssファイル作成、書き込み
+    ③ public以下にファイルcssを作成
+    ④ Webpack.mixに.sass('resources/sass/app.scss', 'public/css');を記入
+    ⑤ npm run watch
+    ⑥ ビューに<link href="{{ asset('css/app.css') }}" rel="stylesheet">を追加
+    全画面がオレンジ色に！！
+  */
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
