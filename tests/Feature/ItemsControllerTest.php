a088172af4291a1669d2acde6cc9409bdfaaa02f
customaddone <custom1485@docomo.ne.jp>
2019-12-03 14:02:20 +0000

    Test login

--- tests/Feature/ItemsControllerTest.php
+++ tests/Feature/ItemsControllerTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Item;
 use App\Http\Controllers\ItemsController;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class ItemsControllerTest extends TestCase
 {
@@ -15,10 +17,15 @@ class ItemsControllerTest extends TestCase
      *
      * @return void
      */
-    public function itemsControllerTest()
+    public function testExample()
     {
         // コントローラーのアクションを使いたい時はItemsControllerインスタンスを生成する
         $item = new ItemsController;
         $this->assertNotEmpty($item->index());
+        // ログイン
+        $user = factory(User::class)->create();
+        $this->be($user);
+        $this->assertNotEmpty(Auth::id());
+
     }
 }
